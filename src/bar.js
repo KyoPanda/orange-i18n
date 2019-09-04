7e4dbb097452c104e58a13f48a94303a9d25061c
Kyo Panda <kyo-panda@hotmail.com>
2019-09-04 17:09:23 -0300

    First commit. :)

---
+++ src/bar.js
@@ -0,0 +1,9 @@
+/**
+ * Return the 'bar' string.
+ *
+ * @module
+ * @returns {string} The string 'bar'.
+ */
+export default function bar() {
+	return 'bar';
+}
---
+++ test/OrangeI18N.test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import bar from '../src/bar';
+
+describe('bar', function () {
+	it('it should return bar', function () {
+		expect(bar()).to.be.equal('bar');
+	});
+});
