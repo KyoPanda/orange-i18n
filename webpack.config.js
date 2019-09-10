e7c1e7987545d85079c284521047a427af5b61d9
Kyo Panda <kyo-panda@hotmail.com>
2019-09-10 13:41:02 -0300

    Force webpack to use production mode only.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+	mode: 'production',
 	target: 'web',
 	entry: './src/index.js',
 	output: {
