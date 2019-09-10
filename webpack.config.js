358bfa2205cc0c5482be613a94a496073092dc73
Kyo Panda <kyo-panda@hotmail.com>
2019-09-10 08:21:25 -0300

    Codecov install on Travis and dist removal.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 	target: 'web',
 	entry: './src/index.js',
 	output: {
